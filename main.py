443b08ff30acd085f6cd49dd8557cd7bc6d52ea4
Zbc <137158632+sissi-s-artlife@users.noreply.github.com>
2023-10-05 20:26:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import time
+from turtle import Screen
+from snake import Snake
+from food import Food
+from scoreboard import Score
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("hungry snake game")
+screen.tracer(0)
+snake = Snake()
+food = Food()
+score = Score()
+
+
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+    #detect collision with food
+    if snake.head.distance(food)<15:
+        food.refresh()
+        score.increase_score()
+        snake.extend()
+    #detect collision with wall
+    if snake.head.xcor() > 299 or snake.head.xcor() < -299 or snake.head.ycor() > 299 or snake.head.ycor() < -299:
+        game_is_on = False
+        score.game_over()
+    #detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            score.game_over()
+            game_is_on = False
+
+
+screen.exitonclick()
